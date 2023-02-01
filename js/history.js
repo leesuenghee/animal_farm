2550feefd5c6bf0e3bca40e5529519e369c51588
mi222ng <mi222ng@naver.com>
2023-02-01 18:58:33 +0900

    history 수정중

--- js/history.js
+++ js/history.js
@@ -1,19 +1,19 @@
-let descs = $("#section2 .history_desc"),
-  descsOST = descs.offset().top,
-  htSticky = $("#section2 .history_title"),
-  htStickyOST = htSticky.offset().top;
-console.log(descsOST);
-$(window).scroll(() => {
-  let sct = $(window).scrollTop();
+// let descs = $("#section2 .history_desc"),
+//   descsOST = descs.offset().top,
+//   htSticky = $("#section2 .history_title"),
+//   htStickyOST = htSticky.offset().top;
+// console.log(descsOST);
+// $(window).scroll(() => {
+//   let sct = $(window).scrollTop();
 
-  if (sct > descsOST) {
-    htSticky.css({
-      position: "skicky",
-      top: "50%",
-      transform: "traslateY(-50%)",
-    });
-  }
-});
+//   if (sct > descsOST) {
+//     htSticky.css({
+//       position: "skicky",
+//       top: "50%",
+//       transform: "traslateY(-50%)",
+//     });
+//   }
+// });
 
 // 스크롤 상단 하단 선 효과
 var s = skrollr.init({
@@ -24,4 +24,13 @@ var s = skrollr.init({
       return 1 - p;
     },
   },
+});
+
+// 0 ~ 해당 섹션 스크롤양 사이에 있을때 a태그에 active가 들어온다
+
+let sections = $("section");
+
+sections.each(function () {
+  sectionOST = $(this).offset().top;
+  console.log(sectionOST);
 });
