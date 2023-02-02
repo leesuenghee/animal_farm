d6e227efb2143f40fc58ee531375b6968bcc403f
mi222ng <mi222ng@naver.com>
2023-02-02 16:45:24 +0900

    history 반응형 수정중

--- js/history.js
+++ js/history.js
@@ -14,6 +14,9 @@
 //     });
 //   }
 // });
+//max-width가 768px 일때 #progress1의 data-end="width:580px;background:hsl(210, 100%, 50%);"로 바뀌어야한다.
+
+
 
 // 스크롤 상단 하단 선 효과
 var s = skrollr.init({
@@ -28,9 +31,16 @@ var s = skrollr.init({
 
 // 0 ~ 해당 섹션 스크롤양 사이에 있을때 a태그에 active가 들어온다
 
-let sections = $("section");
+let sections = $("section"),
+sectionInfo = [],
+  currentIdx = 0;
+  let sct = $(window).scrollTop();
 
 sections.each(function () {
-  sectionOST = $(this).offset().top;
-  console.log(sectionOST);
+  let sectionOST = $(this).offset().top;
+  
+  sectionInfo.push(sectionOST);
+  
 });
+console.log(sct);
+console.log(sectionInfo);
