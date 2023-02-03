6c146c327f971ca19460c160e915fa38d9b302ba
mi222ng <mi222ng@naver.com>
2023-02-03 18:31:21 +0900

    map2 spot 정리하기

--- js/map.js
+++ js/map.js
@@ -1,9 +1,13 @@
-
-
 let tabMenus = $('#tabs .map_submenu a');
 
 tabMenus.click(function(e){
   e.preventDefault();
   tabMenus.removeClass('active');
   $(this).addClass('active');
+  //탭메뉴 컬러 변경
+  $('html, body').animate({
+    scrollTop : 0
+    }, 400);
+    return false;
+    //탭 상단으로 천천히 이동
 });
