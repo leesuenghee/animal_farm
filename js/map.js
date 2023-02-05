74da604e9102b41b429496776dd96bce6aa6c538
mi222ng <mi222ng@naver.com>
2023-02-05 17:46:35 +0900

    map modal 수정중

--- js/map.js
+++ js/map.js
@@ -15,28 +15,48 @@ tabMenus.click(function (e) {
   //탭 상단으로 천천히 이동
 });
 
-// svg spot circle의 index
-// map_list_item의 index
-// 두개의 index 번호가 같다면 map_list_item의 각 내용을 makeHTML에 넣는다.
+//maplist의 각 data-1 내용을 배열로 묶어준다.
 
 let circles = $("svg #spot circle"),
   mapListItem = $(".map_list_item");
-let mapH3 = mapListItem.find("h3"),
-  mapSpan1 = mapListItem.find("span:first-child"),
-  mapSpan2 = mapListItem.find("span:last-child");
 
+let mapH3 = mapListItem.find("h3"),
+  mapSpan1 = mapListItem.find("span:first-of-type"),
+  mapSpan2 = mapListItem.find("span:last-of-type");
+let mapH3Data = [],
+  mapSpan1Data = [],
+  mapSpan2Data = [];
+
+mapH3.each(function () {
+  let mapH3attr = $(this).attr("data-1");
+  mapH3Data.push(mapH3attr);
+});
+mapSpan1.each(function () {
+  let mapSpan1attr = $(this).attr("data-1");
+  mapSpan1Data.push(mapSpan1attr);
+});
+mapSpan2.each(function () {
+  let mapSpan2attr = $(this).attr("data-1");
+  mapSpan2Data.push(mapSpan2attr);
+});
+console.log(mapH3Data);
+console.log(mapSpan1Data);
+console.log(mapSpan2Data);
+
+console.log(mapListItem.length); //21개
+console.log(circles.length); //21개
 //circle을 클릭했을때 할일
 //각 circle의 index번호
 circles.click(function () {
-  $(this).each(function (index) {
-    if (index == mapListItem.eq(index)) {
-    }
-    let makeHTML = `<div>
-      <h3 class="ft32">${$(this).mapH3}</h3>
-      <span>${mapSpan1}</span>
-      <span>${mapSpan2}</span>
-      <img src="" alt="">
-    </div>`;
-    console.log(makeHTML);
-  });
+  if ($(this).eq(0) == mapListItem.length) {
+    makeHTML();
+  }
 });
+
+function makeHTML() {
+  `<div class="map-modal">
+    <h3 class="ft32"></h3>
+    <span></span>
+    <span></span>
+  </div>`;
+}
