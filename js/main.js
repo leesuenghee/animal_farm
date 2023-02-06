e90d0d5c779d6123d0ea79ea8497b3ce1d231756
mi222ng <mi222ng@naver.com>
2023-02-06 14:27:56 +0900

    main 수정

--- js/main.js
+++ js/main.js
@@ -22,36 +22,7 @@ $(".top_btn").click(function (e) {
 });
 
 //메인 슬라이드
-const swiperEl = document.querySelector("swiper-container");
-
-const params = {
-  injectStyles: [
-    `
-      .swiper-pagination-bullet {
-        width: 20px;
-        height: 20px;
-        text-align: center;
-        line-height: 20px;
-        font-size: 12px;
-        color: #000;
-        opacity: 1;
-        background: rgba(0, 0, 0, 0.2);
-      }
-
-      .swiper-pagination-bullet-active {
-        color: #fff;
-        background: #007aff;
-      }
-      `,
-  ],
-  pagination: {
-    clickable: true,
-    renderBullet: function (index, className) {
-      return '<span class="' + className + '">' + (index + 1) + "</span>";
-    },
-  },
-};
-
-Object.assign(swiperEl, params);
-
-swiperEl.initialize();
+$(".swiper").slick({
+  arrows: false,
+  dots: true,
+});
