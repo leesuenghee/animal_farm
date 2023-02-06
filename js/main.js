cc2b3f802d496a21bfde6eacc7a24d5cbd327af6
mi222ng <mi222ng@naver.com>
2023-02-06 11:15:02 +0900

    main slider 수정중

--- js/main.js
+++ js/main.js
@@ -20,3 +20,38 @@ $(".top_btn").click(function (e) {
   e.preventDefault();
   $("html,body").animate({ scrollTop: 0 }, 1000, "easeOutQuint");
 });
+
+//메인 슬라이드
+const swiperEl = document.querySelector("swiper-container");
+
+const params = {
+  injectStyles: [
+    `
+      .swiper-pagination-bullet {
+        width: 20px;
+        height: 20px;
+        text-align: center;
+        line-height: 20px;
+        font-size: 12px;
+        color: #000;
+        opacity: 1;
+        background: rgba(0, 0, 0, 0.2);
+      }
+
+      .swiper-pagination-bullet-active {
+        color: #fff;
+        background: #007aff;
+      }
+      `,
+  ],
+  pagination: {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+};
+
+Object.assign(swiperEl, params);
+
+swiperEl.initialize();
