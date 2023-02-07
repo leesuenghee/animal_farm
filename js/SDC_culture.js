fbb954c7b46363e088beab0431903da8a11ad085
leesuenghee <seng2294@naver.com>
2023-02-07 13:14:27 +0900

    SDC_culture 피드백 수정중

---
+++ js/SDC_culture.js
@@ -0,0 +1,19 @@
+
+
+    new fullpage('#fullpage', {
+        //options here
+        autoScrolling:true,
+        scrollHorizontally: true
+    });
+    
+    
+    $('#fullpage').fullpage({
+        //options here
+        autoScrolling:true,
+        scrollHorizontally: true
+    });
+    
+        //methods
+        // $.fn.fullpage.setAllowScrolling(false);
+    
+
