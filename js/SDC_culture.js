ca5498e28f803adad3d5e8ec7e350e9e35d1aa83
leesuenghee <95907364+leesuenghee@users.noreply.github.com>
2023-02-07 15:27:14 +0900

    SDC_culture 피드백 완료

--- js/SDC_culture.js
+++ js/SDC_culture.js
@@ -1,19 +1,23 @@
-
-
-    new fullpage('#fullpage', {
-        //options here
-        autoScrolling:true,
-        scrollHorizontally: true
+//480이하일 때는 풀페이지 작동 안됨
+$(window).resize(() => {
+  if ($(window).width() >= 480) {
+    new fullpage("#fullpage", {
+      //options here
+      autoScrolling: true,
+      scrollHorizontally: true,
     });
-    
-    
-    $('#fullpage').fullpage({
-        //options here
-        autoScrolling:true,
-        scrollHorizontally: true
-    });
-    
-        //methods
-        // $.fn.fullpage.setAllowScrolling(false);
-    
 
+    $("#fullpage").fullpage({
+      //options here
+      autoScrolling: true,
+      scrollHorizontally: true,
+    });
+    //methods
+    // $.fn.fullpage.setAllowScrolling(false);
+  }
+});
+
+//풀페이지가 되었을 때 header fixed 고정
+// if ($(window).scrollTop() >= 0) {
+//   $("header").css({ position: "fixed", left: 0, right: 0, bottom: 0, top: 0 });
+// }
