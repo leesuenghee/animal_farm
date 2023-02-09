1b9c805f153495069a301f59aa5dc40b4739d641
leesuenghee <95907364+leesuenghee@users.noreply.github.com>
2023-02-09 18:30:28 +0900

    scd 페이지 수정 완료

--- js/SDC_culture.js
+++ js/SDC_culture.js
@@ -1,18 +1,25 @@
 //480이하일 때는 풀페이지 작동 안됨
 $(window).resize(() => {
   if ($(window).width() <= 480) {
-    new fullpage("#fullpage", {
-      //options here
-      autoScrolling: true,
-      scrollHorizontally: true,
-    });
-
-    $("#fullpage").fullpage({
-      //options here
-      autoScrolling: true,
-      scrollHorizontally: true,
-    });
-    //methods
-    // $.fn.fullpage.setAllowScrolling(false);
+    $.fn.fullpage.destroy("all");
+  } else {
+    initFullpage();
   }
+  // if ($(window).width() <= 480) {
+  //   new fullpage("#fullpage", {
+  //     //options here
+  //     autoScrolling: true,
+  //     scrollHorizontally: true,
+  //   });
+  // }
 });
+$(window).trigger("resize");
+ㅛㅅ;
+function initFullpage() {
+  $("#fullpage").fullpage({
+    //options here
+    autoScrolling: true,
+    scrollHorizontally: true,
+  });
+}
+
