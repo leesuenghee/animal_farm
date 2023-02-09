275dbedbfd5e79ffcdbb1fc2b2d3933db038451f
leesuenghee <95907364+leesuenghee@users.noreply.github.com>
2023-02-09 11:07:08 +0900

    기업개요, SDC_culture 페이지 완료

--- js/SDC_culture.js
+++ js/SDC_culture.js
@@ -1,6 +1,6 @@
 //480이하일 때는 풀페이지 작동 안됨
 $(window).resize(() => {
-  if ($(window).width() >= 480) {
+  if ($(window).width() <= 480) {
     new fullpage("#fullpage", {
       //options here
       autoScrolling: true,
