5333e54f2e08d73531fb5b4c98d1c043e8b64e34
CelineSunrise <119560572+CelineSunrise@users.noreply.github.com>
2023-03-05 19:05:56 +0900

    social.js feedback 수정 업로드

--- js/social.js
+++ js/social.js
@@ -22,6 +22,23 @@ $(window).scroll(()=>{
 });
 
 
+/* SECTION2 지역사회활동 */
+let s2p = $('.social_s2 p');
+
+$(window).scroll(()=>{
+    if($(window).scrollTop() > s2p.eq(0).offset().top - 400){
+        s2p.eq(0).addClass('active');
+    }else {
+        s2p.eq(0).removeClass('active');
+    }
+    if($(window).scrollTop() > s2p.eq(1).offset().top - 300){
+        s2p.eq(1).addClass('active');
+    }else{
+        s2p.eq(1).removeClass('active');
+    }
+});
+
+
 /* SECTION3 지역사회활동 */
 let s3SlideWrap = $('.s3_slidewrapper'),
     s3Slides = s3SlideWrap.find('.s3_slides'),
@@ -40,7 +57,7 @@ let s3SlideWrap = $('.s3_slidewrapper'),
         e.preventDefault();
         if(s3CurrentIdx === 0){
             s3Slides.css({left:-s3Slide.length*100+'%'});
-            s3CurrentIdx = s3Slide.length -1;
+            s3CurrentIdx = s3Slide.length;
         }
         s3GotoSlide(s3CurrentIdx-1);
     });
@@ -48,6 +65,7 @@ let s3SlideWrap = $('.s3_slidewrapper'),
         e.preventDefault();
         if(s3CurrentIdx === s3Slide.length - 1){
             s3Slides.css({left:'0%'});
+            s3GotoSlide(0);
             s3CurrentIdx = 0;
         }
         s3GotoSlide(s3CurrentIdx+1);
@@ -66,34 +84,34 @@ let s3SlideWrap = $('.s3_slidewrapper'),
         s3CurrentIdx = i;
     }
 
-/* SECTION4 나눔경영 */
+    /* SECTION4 나눔경영 */
 
-let sc4tWrap = sc4.find('.s4_title_wrapper'),
-    sc4cWrap = sc4.find('.s4_contents_wrapper');
+    let sc4tWrap = sc4.find('.s4_title_wrapper'),
+        sc4cWrap = sc4.find('.s4_contents_wrapper');
 
-// $(window).scroll(()=>{
-//     if($(this).scrollTop() >= sc4.offset().top){
-//         sc4tWrap.css({position:'sticky'});
-//     }else if($(this).scrollTop() >= sc4cWrap.offset().bottom){
-//         sc4.css({position:'relative'});
-//     }
-// });
+    // $(window).scroll(()=>{
+    //     if($(this).scrollTop() >= sc4.offset().top){
+    //         sc4tWrap.css({position:'sticky'});
+    //     }else if($(this).scrollTop() >= sc4cWrap.offset().bottom){
+    //         sc4.css({position:'relative'});
+    //     }
+    // });
 
-var swiper = new Swiper(".s4_slidewrapper .mySwiper", {
-  spaceBetween: 10,
-  slidesPerView: 2,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    768: {
-      slidesPerView: 4,
-      spaceBetween: 40,
+    var swiper = new Swiper(".s4_slidewrapper .mySwiper", {
+    spaceBetween: 10,
+    slidesPerView: 2,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
     },
-    480: {
-      slidesPerView: 3,
-      spaceBetween: 20
+    breakpoints: {
+        768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+        },
+        480: {
+        slidesPerView: 3,
+        spaceBetween: 20
+        }
     }
-  }
-});
+    });
